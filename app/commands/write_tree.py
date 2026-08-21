edcde260713bc14a8441e3231df95ec13439ff59
nandbit <nikitagalayda@gmail.com>
2026-08-21 18:31:06 +0800

    codecrafters submit [skip ci]

--- app/commands/write_tree.py
+++ app/commands/write_tree.py
@@ -1,4 +1,5 @@
 import os
+import zlib
 from hashlib import sha1
 from pathlib import Path
 
@@ -12,15 +13,32 @@ def write_tree() -> None:
 
 
 def _get_staged_targets(directory: str) -> list[str]:
-    output = ""
+    entries = dict()
     for dirpath, dirnames, filenames in os.walk(directory):
         for f in filenames:
-            output += _create_file_string(Path(os.path.join(dirpath, f)))
+            filepath = Path(os.path.join(dirpath, f))
+            mode, hash = _process_file(filepath)
+            entries[f] = {"hash": hash, "mode": mode}
         for d in dirnames:
             if d == ".git":
                 continue
-            output += _get_staged_targets(Path(os.path.join(directory, d)))
+            dirpath = Path(os.path.join(directory, d))
+            hash = _get_staged_targets(dirpath)
+            entries[d] = {"hash": hash, "mode": "40000"}
         break
+
+    # for k, v in entries.items():
+    #     print(f"{k}: hash: {v['hash'][:5]} mode: {v['mode']}")
+
+    entries_sorted = {k: v for k, v in sorted(entries.items())}
+
+    entries_list = [
+        f"{v['mode']} {k}\0{v['hash'][:20]}" for k, v in entries_sorted.items()
+    ]
+    entries_string = "".join(entries_list)
+    header = f"tree {len(entries_string)}\0"
+    output = header + entries_string
+    output_compressed = zlib.compress(output.encode())
 
     h = sha1()
     h.update(output.encode())
@@ -28,24 +46,20 @@ def _get_staged_targets(directory: str) -> list[str]:
     new_tree_object_dir = f".git/objects/{new_tree_object_hash[:2]}"
     new_tree_object_path = f"{new_tree_object_dir}/{new_tree_object_hash[2:]}"
 
-    print(f"creating directory {new_tree_object_dir}")
     os.makedirs(new_tree_object_dir, exist_ok=True)
+    with open(new_tree_object_path, "wb") as f:
+        f.write(output_compressed)
 
-    print(f"writing to file {new_tree_object_path}")
-    with open(new_tree_object_path, "w") as f:
-        f.write(output)
-
-    print(output)
-
-    return hash_object(
-        target=new_tree_object_path,
-        write=False,
-        stdin=False,
-        content_type="tree",
-    )
+    return new_tree_object_hash
+    # return hash_object(
+    #     target=new_tree_object_path,
+    #     write=False,
+    #     stdin=False,
+    #     content_type="tree",
+    # )
 
 
-def _create_file_string(filepath: Path) -> str:
+def _process_file(filepath: Path) -> tuple[str, str]:
     mode = get_file_mode(filepath)
     hash = hash_object(
         target=filepath,
@@ -54,4 +68,4 @@ def _create_file_string(filepath: Path) -> str:
         content_type="blob",
     )
 
-    return f"{mode} {filepath.name}\0{hash}"
+    return mode, hash
